760394ec53a6a7b62ee148880bec0a1a1fb9407f
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-08-08 22:52:48 -0600

    updated DataAccess with info from the new model package

--- server/src/main/java/server/dataaccess/DataAccess.java
+++ server/src/main/java/server/dataaccess/DataAccess.java
@@ -1,4 +1,19 @@
 package server.dataaccess;
+import model.*;
+import java.util.List;
 
-public interface DataAccess { void clear();
+public interface DataAccess {
+    void clear();
+    void create(UserData u) throws DataAccessException;
+    UserData getUser(String username);
+
+    void createAuth (AuthData a);
+    AuthData getAuth (String token);
+    void deleteAuth (String token);
+
+    int createGame(String gameName) throws DataAccessException;
+    GameData getGame(int id);
+    List<GameData> listGames();
+    void saveGame(GameData g) throws DataAccessException;
+
 }
