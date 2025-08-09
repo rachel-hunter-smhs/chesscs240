0ed543e04d03173c68daa0aea566c54ce033eb69
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-08-08 23:57:23 -0600

    updating UserService

--- server/src/main/java/server/service/UserService.java
+++ server/src/main/java/server/service/UserService.java
@@ -1,4 +1,14 @@
 package server.service;
+import server.dataaccess.DataAccess;
+import server.dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import java.util.UUID;
 
 public class UserService {
+    private final DataAccess dao;
+    public UserService (DataAccess d){ dao = d;}
+
+    public record RegisterRequest(String username,String password,String email){}
+    public record RegisterResult
 }
