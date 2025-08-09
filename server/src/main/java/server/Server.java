473e840a78f8c4cba7e6a59b126b1b687f400a27
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-08-08 21:24:07 -0600

    added import statements to server (looked up what I would need)

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,28 @@
 package server;
 
 import spark.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import service.ClearService;
+import service.GameService;
+import service.UserService;
+import spark.Response;
+import spark.Spark;
 
 public class Server {
+    private final Gson gson = new Gson();
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
-
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        var dao  = new MemoryDataAccess();
+        var users = new UserService(dao);
+        var games = new GameService(dao);
+        var clear = new ClearService(dao);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
