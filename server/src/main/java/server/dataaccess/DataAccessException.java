96f8bb0ab62a46a3ea484fc6aa6ac2a863ed42d7
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-08-11 02:55:47 -0600

    downloaded and started with starter files

--- server/src/main/java/server/dataaccess/DataAccessException.java
+++ server/src/main/java/server/dataaccess/DataAccessException.java
@@ -1,4 +1,11 @@
 package server.dataaccess;
 
-public class DataAccessException extends Exception { public DataAccessException(String m){ super(m);}
+public class DataAccessException extends Exception {
+    public DataAccessException(String message) {
+        super(message);
+    }
+    public DataAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
+
