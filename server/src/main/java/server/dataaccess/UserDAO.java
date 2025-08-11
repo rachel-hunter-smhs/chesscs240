f3b31a60666429df2fcdc3acbfec7390a9282cb7
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-08-11 15:32:01 -0600

    updated DatabaseManager.java

---
+++ server/src/main/java/server/dataaccess/UserDAO.java
@@ -0,0 +1,12 @@
+package server.dataaccess;
+
+import model.UserData;
+import java.util.List;
+
+public interface UserDAO {
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    List<UserData> listUsers() throws DataAccessException;
+    void deleteUser(String username) throws DataAccessException;
+}
+
---
+++ server/src/main/java/server/dataaccess/mysql/MySqlUserDAO.java
@@ -0,0 +1,8 @@
+package server.dataaccess.mysql;
+import server.dataaccess.DatabaseManager;
+import server.dataaccess.UserDAO;
+import model.*;
+import server.dataaccess.DataAccessException;
+
+public class MySqlUserDAO {
+}
