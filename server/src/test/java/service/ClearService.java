fbe61dc9636e4aa7a947f76600b0570f41e28498
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-11-15 16:00:00 -0700

    Removed double code.

--- server/src/test/java/service/ClearService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import dataaccess.DataAccess;
-
-public class ClearService {
-    private final DataAccess dao;
-    public ClearService(DataAccess dao) { this.dao = dao; }
-    public void clear() { dao.clear(); }
-}
