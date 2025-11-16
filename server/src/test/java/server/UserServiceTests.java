d5d7f540b972e37ab9f66245f6e09072317ab437
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-11-16 16:33:37 -0700

    Wrote the import statements

--- server/src/test/java/server/UserServiceTests.java
+++ server/src/test/java/server/UserServiceTests.java
@@ -1,4 +1,10 @@
 package server;
 
+import org.junit.jupiter.api.*;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import service.UserService;
+
 public class UserServiceTests {
 }
