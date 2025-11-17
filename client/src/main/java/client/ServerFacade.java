7f838b808c711928294863f97c28dcdd556e60d6
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-11-16 19:45:16 -0700

    Deleted empty accident file. Added import statement to server facade.

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,9 @@
+package client;
+import com.google.gson.Gson;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ServerFacade {
+}
