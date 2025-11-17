5437cfefa4c55ca037655482025176a9029af702
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-11-16 19:57:36 -0700

    creates the basic variables and intializes them

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -6,4 +6,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ServerFacade {
+    private final String serverLink;
+    private final HttpClient httpClient;
+    private final Gson gson = new Gson();
+
+    public ServerFacade(int port) {
+        this.serverLink = "http://localhost:" + port;
+        this.httpClient = HttpClient.newHttpClient();
+    }
 }
