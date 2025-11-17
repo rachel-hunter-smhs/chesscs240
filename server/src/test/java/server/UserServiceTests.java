01f52c787920dbd1f16b63b79dd6c3a8c4564366
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-11-16 17:06:18 -0700

    Added test for same username and same email

--- server/src/test/java/server/UserServiceTests.java
+++ server/src/test/java/server/UserServiceTests.java
@@ -29,4 +29,26 @@ public class UserServiceTests {
     void registerNullRegistrationRequest(){
         Assertions.assertThrows(DataAccessException.class,()->userService.register(null));
     }
+    @Test
+    void registerSameUsername() throws DataAccessException{
+        var firstUser = new UserService.RegisterRequest("Spongebob", "BikiniBottom", "squarepants@krustykrab.com");
+        userService.register(firstUser);
+        var secondUser = new UserService.RegisterRequest("Spongebob", "GaryDaSn@il", "spongebobsquarepants@krustykrab.com");
+        Assertions.assertThrows(DataAccessException.class, () -> userService.register(secondUser));
+    }
+
+    @Test
+    void registerSameEmail() throws DataAccessException {
+        var firstUser = new UserService.RegisterRequest("Spongebob", "BikiniBottom", "squarepants@krustykrab.com");
+        var result = userService.register(firstUser);
+        Assertions.assertEquals("Spongebob", result.username());
+        Assertions.assertNotNull(result.authToken());
+        Assertions.assertTrue(result.authToken().length() > 5);
+        var secondUser = new UserService.RegisterRequest("Squidward", "GaryDaSn@il", "squarepants@krustykrab.com");
+        var resultTwo= userService.register(secondUser);
+        Assertions.assertEquals("Squidward", resultTwo.username());
+        Assertions.assertNotNull(resultTwo.authToken());
+        Assertions.assertTrue(resultTwo.authToken().length() > 5);
+
+    }
 }
