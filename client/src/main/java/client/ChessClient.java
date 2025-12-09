e0836259a5a2eb130f35949b0b980790bc5f6a28
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-12-09 07:05:15 -0700

    Setting up ChessClient

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -13,4 +13,42 @@ public class ChessClient {
     public ChessClient(ServerFacade server){
         this.server = server;
     }
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type 'help' to start");
+        while (true){
+            printPrompt();
+            String line = scanner.nextLine().trim();
+
+            if (line.isEmpty()){
+                continue;
+            }
+            try {
+                if (eval(line)){
+                    break;
+                }
+            } catch (Exception e){
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+    private void printPrompt(){
+        if(state == State.PRELOGIN){
+            System.out.print("[LOGGED OUT] >>>");
+        } else{
+            System.out.print("[LOGGED IN] >>>");
+        }
+    }
+    private boolean eval(String input) throws Exception{
+        String[] tokens = input.split("\\s+");
+        String cmd = tokens[0].toLowerCase();
+
+        if(state == State.PRELOGIN {
+            return evalPrelogin(cmd, tokens);
+        } else{
+            return evalPostlogin(cmd, tokens);
+        }
+    }
+
 }
