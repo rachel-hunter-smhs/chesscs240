e649d5999b46a32b0421f263f6158359bd16dd48
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-12-10 03:00:02 -0700

    getters and setters for GameCommandUser

--- shared/src/main/java/websocket/commands/GameCommandUser.java
+++ shared/src/main/java/websocket/commands/GameCommandUser.java
@@ -1,4 +1,29 @@
 package websocket.commands;
 
 public class GameCommandUser {
+    public  enum  CommandType{
+        CONNECT,
+        MAKE_MOVE,
+        LEAVE,
+        RESIGN
+    }
+    protected CommandType commandType;
+    protected String authToken;
+    protected Integer gameID;
+    public GameCommandUser(CommandType commandType, String authToken, Integer gameID){
+        this.authToken = authToken;
+        this.commandType = commandType;
+        this.gameID = gameID;
+
+    }
+
+    public CommandType getCommandType() {
+        return commandType;
+    }
+    public String getAuthToken(){
+        return authToken;
+    }
+    public Integer getGameID(){
+        return gameID;
+    }
 }
