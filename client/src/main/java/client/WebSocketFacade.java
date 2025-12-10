59cc261623d6c141eb7a2736a74d2df11ff2d0b7
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-12-10 03:08:43 -0700

    updated variables and imprts

--- client/src/main/java/client/WebSocketFacade.java
+++ client/src/main/java/client/WebSocketFacade.java
@@ -3,6 +3,15 @@ package client;
 import com.google.gson.Gson;
 import websocket.commands.GameCommandUser;
 import websocket.message.ServerMessage;
-
+import javax.websocket.*;
+import java.net.URI;
+@ClientEndpoint
 public class WebSocketFacade {
+    private Session sesh;
+    private final Gson gson = new Gson();
+    public WebSocketFacade(String url) throws Exception{
+        URI uri = new URI(url);
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.sesh = container.connectToServer(this, uri);
+    }
 }
