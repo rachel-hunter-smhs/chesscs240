a9aaeb32e2da2b7efc8dd1352b6aa8dc5840a6b3
rachel-hunter-smhs <rachel.hunter2001@gmail.com>
2025-12-10 02:20:00 -0700

    websocketHandler add more functions

--- server/src/main/java/server/websocket/WebsocketHandler.java
+++ server/src/main/java/server/websocket/WebsocketHandler.java
@@ -12,5 +12,13 @@ public class WebsocketHandler {
         System.out.println("Websocket connection: " + session.getRemoteAddress()) ;
     }
     @OnWebSocketClose
-    public void onClose
+    public void onClose(Session session, int codeStat, String reasoning){
+        System.out.println("Websocket closed" + session.getRemoteAddress());
+    }
+    @OnWebSocketMessage
+    public void onMessage(Session session, String stat){
+        System.out.println("Recieved Message: " + stat);
+        //session.getRemote().sendString("Echo: " + stat);
+
+    }
 }
